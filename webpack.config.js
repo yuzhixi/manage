07da586883be16a854e02d475a649b64ae67df3c
yuzhixi <1020503098@qq.com>
2019-05-27 23:35:56 +0800

    add package

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const webpack  = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'js/app.js'
+  },
+  module: {
+    rules: [
+        {
+            test: /\.m?jsx$/,
+            exclude: /(node_modules)/,
+            use: {
+            loader: 'babel-loader',
+            options: {
+                //env会根据node或浏览器环境自动配置打包
+                presets: ['env', 'react']
+            }
+            }
+        },
+        {
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            })
+        },
+        {
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: ["css-loader", "sass-loader"]
+            })
+        },
+        {
+            test: /\.(png|jpg|gif)$/i,
+            use: [
+                {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        name: 'resource/[name].[ext]'
+                    }
+                }
+            ]
+        },
+        {
+            test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+            use: [
+                {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                        name: 'resource/[name].[ext]'
+                    }
+                }
+            ]
+        }
+    ]
+  },
+  plugins: [
+      //处理独立文件
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        // 独立css文件
+        new ExtractTextPlugin('css/[name].css'),
+        //提出公共模块
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'js/base.js'
+        })
+    ],
+    devServer: {
+        port: 8086
+        // contentBase: './dist'
+    }
+};
